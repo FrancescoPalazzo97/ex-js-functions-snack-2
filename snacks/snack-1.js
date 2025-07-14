50311d9cdacc1722ee62fb2a69a8e50f8c93e6d0
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-14 20:35:35 +0200

    UPD - Snack 1 variant done

--- snacks/snack-1.js
+++ snacks/snack-1.js
@@ -1,5 +1,11 @@
-function somma(num1, num2) {
-    return num1 + num2;
-}
+// function somma(num1, num2) {
+//     return num1 + num2;
+// }
 
-console.log(somma(2, 2));
+// console.log(somma(2, 2));
+
+// Arrow function
+
+const somma = (num1, num2) => num1 + num2;
+
+console.log(somma(4, 8));
